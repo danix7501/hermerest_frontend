4e81fde5eee9edfb6aa8e1009d8ffa85e549c343
Daniel Romero Calero <romerocalerod@gmail.com>
2018-04-14 17:46:15 +0100

    Interfaz de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppComponent } from './app.component';
-
+import {LoginModule} from './pages/login/login.module';
+import {appRoutingProviders, routing} from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    routing
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [appRoutingProviders],
+  bootstrap: [AppComponent],
+  exports: [BsDropdownModule, TooltipModule, ModalModule]
 })
 export class AppModule { }
